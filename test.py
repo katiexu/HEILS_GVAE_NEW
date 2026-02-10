f59bbe4998a3942674d26a006a3cbcbe8d112675
katie <xuke0531@hotmail.com>
2026-02-10 11:16:07 +0800

    StatevectorSimulator  (Estimator+GenericBackendV2)

---
+++ test.py
@@ -0,0 +1,6 @@
+import qiskit
+import qiskit_aer
+import qiskit_machine_learning
+print(f"Qiskit主版本: {qiskit.__version__}")
+print(f"qiskit_aer: {qiskit_aer.__version__}")
+print(f"qiskit_machine_learning: {qiskit_machine_learning.__version__}")
